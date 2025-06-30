aac5b8b31857840c95e92edbb87fef9eebf0fde9
beambeambeam <supawitmarayat@gmail.com>
2025-06-30 21:33:52 +0700

    feat: router

--- packages/api/api/v1/routers/__init__.py
+++ packages/api/api/v1/routers/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from ...auth.router import router as auth_router
+from ...collection.router import router as collection_router
 from .health import router as health_router
 
 api_router = APIRouter()
 api_router.include_router(health_router)
 api_router.include_router(auth_router)
+api_router.include_router(collection_router)
