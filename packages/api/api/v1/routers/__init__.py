c51c40d5e870271187f58a579ba069b039bfdfd5
beambeambeam <supawitmarayat@gmail.com>
2025-07-01 00:06:15 +0700

    chore: remove document history

--- packages/api/api/v1/routers/__init__.py
+++ packages/api/api/v1/routers/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from ...auth.router import router as auth_router
 from ...collection.router import router as collection_router
 from ...document.router import router as document_router
-from ...storage.router import router as storage_router
 from .health import router as health_router
 
 api_router = APIRouter()
@@ -11,4 +10,3 @@ api_router.include_router(health_router)
 api_router.include_router(auth_router)
 api_router.include_router(collection_router)
 api_router.include_router(document_router)
-api_router.include_router(storage_router)
