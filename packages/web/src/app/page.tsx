92a02b887aa8ac282e2e331f872805f91d93a8ec
beambeambeam <supawitmarayat@gmail.com>
2025-07-03 18:16:17 +0700

    feat: enhance Index component layout with responsive design and navigation link

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Index() {
-  return <div>The Codex</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center flex-col gap-y-4">
+      <h1 className="text-4xl">The Codex</h1>
+      <Link href="/sign-in">
+        <Button>Let&apos;s get start!</Button>
+      </Link>
+    </div>
+  );
 }
