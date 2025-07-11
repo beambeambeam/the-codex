dbd294dc3e2ee701373a173c002597e7c48a5cb4
beambeambeam <supawitmarayat@gmail.com>
2025-07-12 01:17:57 +0700

    feat: implement CollectionFileForm component for file uploads in CollectionIdFilePage

--- packages/web/src/app/(protected)/collection/[id]/files/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/files/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import CollectionFileForm from "@/app/(protected)/collection/[id]/files/_components/form";
+
 function CollectionIdFilePage() {
   return (
     <div className="h-full w-full border-l p-4">
       <div className="text-2xl font-bold">Manage all files</div>
+      <CollectionFileForm
+        defaultValues={{
+          files: [],
+        }}
+        onSubmit={function (values: { files: File[] }): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
     </div>
   );
 }
