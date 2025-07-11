020433d23a7ae43a999ed7f001576d4540c78984
beambeambeam <supawitmarayat@gmail.com>
2025-07-12 00:48:16 +0700

    feat: update CollectionIdFilePage to display 'Manage all files' header and add FileUpload component

--- packages/web/src/app/(protected)/collection/[id]/files/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/files/page.tsx
@@ -1,4 +1,8 @@
 function CollectionIdFilePage() {
-  return <div className="h-full w-full border-l p-4">CollectionIdFilePage</div>;
+  return (
+    <div className="h-full w-full border-l p-4">
+      <div className="text-2xl font-bold">Manage all files</div>
+    </div>
+  );
 }
 export default CollectionIdFilePage;
