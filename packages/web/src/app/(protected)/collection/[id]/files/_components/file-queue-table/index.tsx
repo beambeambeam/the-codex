0a51c5cff477a080b9839df4e4cac88b037effc4
beambeambeam <supawitmarayat@gmail.com>
2025-07-12 02:33:51 +0700

    feat: advance filter

--- packages/web/src/app/(protected)/collection/[id]/files/_components/file-queue-table/index.tsx
+++ packages/web/src/app/(protected)/collection/[id]/files/_components/file-queue-table/index.tsx
@@ -1,18 +1,27 @@
 import { fileQueueColumns } from "@/app/(protected)/collection/[id]/files/_components/file-queue-table/columns";
 import { DataTable } from "@/components/data-table/data-table";
+import { DataTableFilterList } from "@/components/data-table/data-table-filter-list";
 import { DataTableToolbar } from "@/components/data-table/data-table-toolbar";
 import { useDataTable } from "@/hooks/use-data-table";
 
 function FileQueueTable() {
   const { table } = useDataTable({
-    data: [],
+    data: [
+      {
+        id: "d",
+        name: "d",
+      },
+    ],
     columns: fileQueueColumns,
     pageCount: 10,
     getRowId: (row) => row.id,
   });
+
   return (
     <DataTable table={table}>
-      <DataTableToolbar table={table}></DataTableToolbar>
+      <DataTableToolbar table={table}>
+        <DataTableFilterList table={table} />
+      </DataTableToolbar>
     </DataTable>
   );
 }
