197fea8257bcc941517ca6a0a89a0979d63e7318
beambeambeam <supawitmarayat@gmail.com>
2025-07-12 02:41:22 +0700

    feat: enhance layout and headings in CollectionIdFilePage for better user experience

--- packages/web/src/app/(protected)/collection/[id]/files/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/files/page.tsx
@@ -2,20 +2,30 @@
 
 import FileQueueTable from "@/app/(protected)/collection/[id]/files/_components/file-queue-table";
 import CollectionFileForm from "@/app/(protected)/collection/[id]/files/_components/form";
+import { Label } from "@/components/ui/label";
 
 function CollectionIdFilePage() {
   return (
-    <div className="h-full w-full border-l p-4">
-      <div className="text-2xl font-bold">Manage all files</div>
-      <CollectionFileForm
-        defaultValues={{
-          files: [],
-        }}
-        onSubmit={function (values: { files: File[] }): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
-      <FileQueueTable />
+    <div className="flex h-full w-full flex-col gap-6 border-l p-4">
+      <div className="text-4xl font-bold">Manage doucuments</div>
+      <div className="flex flex-col">
+        <div className="text-2xl font-bold">
+          Upload documents to collections
+        </div>
+        <CollectionFileForm
+          defaultValues={{
+            files: [],
+          }}
+          onSubmit={function (values: { files: File[] }): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="text-2xl font-bold">Documents Queue</div>
+        <Label>View all files currently being processed / done processed</Label>
+        <FileQueueTable />
+      </div>
     </div>
   );
 }
