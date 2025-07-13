10dffa765f4f3de57e48ea10a83d9688dc81fd2b
beambeambeam <supawitmarayat@gmail.com>
2025-07-13 17:19:12 +0700

    feat: personalize greeting in HomePage with username from user store

--- packages/web/src/app/(protected)/home/page.tsx
+++ packages/web/src/app/(protected)/home/page.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import HomeCanvas from "@/app/(protected)/home/_components/canvas";
 import { ToggleThemeButton } from "@/components/button/toggle-theme";
 import SettingDialog from "@/components/settings/dialog";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useUser } from "@/store/userStore";
 
 function HomePage() {
+  const { username } = useUser();
   return (
     <div className="flex h-full w-full flex-col items-start justify-center gap-4">
       <div className="flex w-full items-center justify-between">
         <div className="flex items-start gap-2">
           <SidebarTrigger />
           <div className="flex flex-col text-2xl font-semibold">
-            <h1>What are you</h1>
-            <h1>interest in today?</h1>
+            <h1 className="capitalize">Hello {username}!</h1>
+            <h1>What are you interest in today?</h1>
           </div>
         </div>
 
