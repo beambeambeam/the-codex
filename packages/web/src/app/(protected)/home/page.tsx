5833a9640443ceb5f1335b201c8752306623cd97
beambeambeam <supawitmarayat@gmail.com>
2025-07-13 18:03:36 +0700

    feat: add loading state to HomePage with Loader component

--- packages/web/src/app/(protected)/home/page.tsx
+++ packages/web/src/app/(protected)/home/page.tsx
@@ -6,6 +6,7 @@ import HomeCanvas from "@/app/(protected)/home/_components/canvas";
 import { useHomeActions } from "@/app/(protected)/home/_components/context";
 import { ToggleThemeButton } from "@/components/button/toggle-theme";
 import SettingDialog from "@/components/settings/dialog";
+import { Loader } from "@/components/ui/loader";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { $api } from "@/lib/api/client";
 import { useUser } from "@/store/userStore";
@@ -40,7 +41,13 @@ function HomePage() {
         </div>
       </div>
       {isPending ? (
-        <div></div>
+        <div className="border-foreground/20 flex h-full w-full items-center justify-center border">
+          <Loader
+            variant="text-shimmer"
+            text="Fetching your collections..."
+            className="text-2xl font-bold"
+          />
+        </div>
       ) : (
         <HomeCanvas
           nodes={(data ?? []).map((item) => ({
