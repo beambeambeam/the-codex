4e50bdc155d02f76bc8eafa02e83e1712b9f2458
beambeambeam <supawitmarayat@gmail.com>
2025-07-15 23:00:15 +0700

    refactor: comment out unused authentication logic in middleware

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -6,23 +6,23 @@ const authPages = ["/sign-in", "/register"];
 const protectedPages = ["/collections/*"];
 
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const { data: user } = await fetchServer.GET("/auth/me");
+  // const { pathname } = request.nextUrl;
+  // const { data: user } = await fetchServer.GET("/auth/me");
 
-  if (authPages.includes(pathname)) {
-    if (user) {
-      return NextResponse.redirect(new URL("/home", request.url));
-    }
-  }
+  // if (authPages.includes(pathname)) {
+  //   if (user) {
+  //     return NextResponse.redirect(new URL("/home", request.url));
+  //   }
+  // }
 
-  for (const protectedPage of protectedPages) {
-    const pattern = new RegExp("^" + protectedPage.replace("*", ".*") + "$");
-    if (pattern.test(pathname)) {
-      if (!user) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-      }
-    }
-  }
+  // for (const protectedPage of protectedPages) {
+  //   const pattern = new RegExp("^" + protectedPage.replace("*", ".*") + "$");
+  //   if (pattern.test(pathname)) {
+  //     if (!user) {
+  //       return NextResponse.redirect(new URL("/sign-in", request.url));
+  //     }
+  //   }
+  // }
 
   return NextResponse.next();
 }
