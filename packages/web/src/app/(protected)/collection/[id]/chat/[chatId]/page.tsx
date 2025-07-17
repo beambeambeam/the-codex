5f69f4dde9958710e79c2a762f6e4a1e194098f1
beambeambeam <supawitmarayat@gmail.com>
2025-07-17 14:02:29 +0700

    feat: refactor chat components layout and add ChatLayout for better structure

--- packages/web/src/app/(protected)/collection/[id]/chat/[chatId]/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/chat/[chatId]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { MOCK_CHAT_HISTORY } from "@/app/(protected)/collection/[id]/__mock__/chat";
-import CollectionIdTabs from "@/app/(protected)/collection/[id]/_components/tabs";
 import ChatForm, {
   ChatFormSchemaType,
 } from "@/app/(protected)/collection/[id]/chat/_components/chat-form";
@@ -10,27 +9,24 @@ import ChatTemplate from "@/app/(protected)/collection/[id]/chat/_components/cha
 function CollectionIdPage() {
   return (
     <>
-      <CollectionIdTabs tab="tab-chat" />
-      <div className="bg-background flex h-full w-full flex-col border-l">
-        <div className="h-full w-full">
-          <header className="relative z-20 mb-4 border-b p-4">
-            {MOCK_CHAT_HISTORY.title}
-          </header>
-          <ChatTemplate message={MOCK_CHAT_HISTORY.history} />
-        </div>
-        <div className="absolute right-0 bottom-0 left-0 z-10 flex flex-1 flex-col justify-end p-10">
-          <ChatForm
-            onSubmit={function (
-              values: ChatFormSchemaType,
-            ): void | Promise<void> {
-              throw new Error("Function not implemented.");
-            }}
-            defaultValues={{
-              chat_message: "",
-            }}
-            suggest={false}
-          />
-        </div>
+      <div className="h-full w-full">
+        <header className="relative z-20 mb-4 border-b p-4">
+          {MOCK_CHAT_HISTORY.title}
+        </header>
+        <ChatTemplate message={MOCK_CHAT_HISTORY.history} />
+      </div>
+      <div className="absolute right-0 bottom-0 left-0 z-10 flex flex-1 flex-col justify-end p-10">
+        <ChatForm
+          onSubmit={function (
+            values: ChatFormSchemaType,
+          ): void | Promise<void> {
+            throw new Error("Function not implemented.");
+          }}
+          defaultValues={{
+            chat_message: "",
+          }}
+          suggest={false}
+        />
       </div>
     </>
   );
--- packages/web/src/app/(protected)/collection/[id]/chat/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/chat/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import CollectionIdTabs from "@/app/(protected)/collection/[id]/_components/tabs";
 import ChatForm, {
   ChatFormSchemaType,
 } from "@/app/(protected)/collection/[id]/chat/_components/chat-form";
@@ -9,27 +8,24 @@ import ChatTemplate from "@/app/(protected)/collection/[id]/chat/_components/cha
 function ChatPage() {
   return (
     <>
-      <CollectionIdTabs tab="tab-chat" />
-      <div className="bg-background flex h-full w-full flex-col border-l">
-        <div className="h-full w-full">
-          <header className="relative z-20 border-b p-4">
-            Start New Conversation
-          </header>
-          <ChatTemplate message={[]} />
-        </div>
-        <div className="absolute right-0 bottom-0 left-0 z-10 flex flex-1 flex-col justify-end p-10">
-          <ChatForm
-            onSubmit={function (
-              values: ChatFormSchemaType,
-            ): void | Promise<void> {
-              throw new Error("Function not implemented.");
-            }}
-            defaultValues={{
-              chat_message: "",
-            }}
-            suggest={true}
-          />
-        </div>
+      <div className="h-full w-full">
+        <header className="relative z-20 border-b p-4">
+          Start New Conversation
+        </header>
+        <ChatTemplate message={[]} />
+      </div>
+      <div className="absolute right-0 bottom-0 left-0 z-10 flex flex-1 flex-col justify-end p-10">
+        <ChatForm
+          onSubmit={function (
+            values: ChatFormSchemaType,
+          ): void | Promise<void> {
+            throw new Error("Function not implemented.");
+          }}
+          defaultValues={{
+            chat_message: "",
+          }}
+          suggest={true}
+        />
       </div>
     </>
   );
