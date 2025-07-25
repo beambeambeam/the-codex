6137d497d91791b65da96085896a3d5c61af33ba
beambeambeam <supawitmarayat@gmail.com>
2025-07-25 19:53:03 +0700

    feat: simplify Page layout and improve component structure

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -5,22 +5,14 @@ import { Button } from "@/components/ui/button";
 
 function Page() {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <div className="absolute z-10">
-        <div className="mx-36 flex h-screen w-screen flex-col items-start justify-center gap-y-6">
-          <Logo size={400} />
-          <div className="flex flex-col gap-2 text-6xl font-bold">
-            Make every data, Askable.
-          </div>
-          <Link href="/sign-in">
-            <Button variant="outline">Let&apos;s get start!</Button>
-          </Link>
-        </div>
-        <Logo size={400} />
-        <Link href="/sign-in">
-          <Button>Let&apos;s get start!</Button>
-        </Link>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <Logo size={400} />
+      <div className="flex flex-col gap-2 text-6xl font-bold">
+        Make every data, Askable.
       </div>
+      <Link href="/sign-in">
+        <Button variant="outline">Let&apos;s get start!</Button>
+      </Link>
     </div>
   );
 }
