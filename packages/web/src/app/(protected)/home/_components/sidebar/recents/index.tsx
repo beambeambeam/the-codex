26e8780197e4914e94e28540d9d6a2bd59792f76
beambeambeam <supawitmarayat@gmail.com>
2025-07-25 12:31:51 +0700

    refactor: update HomeSidebarRecents component structure and remove console log from HomePage

--- packages/web/src/app/(protected)/home/_components/sidebar/recents/index.tsx
+++ packages/web/src/app/(protected)/home/_components/sidebar/recents/index.tsx
@@ -15,27 +15,35 @@ interface HomeSidebarRecentProps {
 function HomeSidebarRecents(props: HomeSidebarRecentProps) {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-foreground/60 text-sm font-bold">Collection Trees!</p>
+      <p className="text-foreground/60 text-sm font-bold">Recents Collection</p>
       <Scroller
         className="flex h-full max-h-[calc(100vh-30rem))] flex-col gap-2"
         hideScrollbar
         withNavigation
       >
-        {props.links.map((link) => (
-          <Link href={link.href} key={link.href}>
-            <Card
-              key={link.href}
-              className="hover:bg-muted cursor-pointer py-3 shadow-none transition-colors"
-            >
-              <CardContent className="flex items-center justify-between overflow-hidden px-4 text-sm">
-                <span className="block max-w-[14rem] truncate">
-                  {link.title}
-                </span>
-                {link.starred && <StarIcon className="size-4" />}
-              </CardContent>
-            </Card>
-          </Link>
-        ))}
+        {props.links.length === 0 ? (
+          <Card className="shadow-none">
+            <CardContent className="text-muted-foreground w-full py-3 text-center text-sm">
+              No recent collections found.
+            </CardContent>
+          </Card>
+        ) : (
+          props.links.map((link) => (
+            <Link href={link.href} key={link.href}>
+              <Card
+                key={link.href}
+                className="hover:bg-muted cursor-pointer py-3 shadow-none transition-colors"
+              >
+                <CardContent className="flex items-center justify-between overflow-hidden px-4 text-sm">
+                  <span className="block max-w-[14rem] truncate">
+                    {link.title}
+                  </span>
+                  {link.starred && <StarIcon className="size-4" />}
+                </CardContent>
+              </Card>
+            </Link>
+          ))
+        )}
       </Scroller>
     </div>
   );
