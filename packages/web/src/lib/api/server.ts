5d27c00572c24b87a0fae73abc7c38191e5be34b
beambeambeam <supawitmarayat@gmail.com>
2025-07-30 20:54:02 +0700

    fix: ensure credentials are included in fetch requests for server API

--- packages/web/src/lib/api/server.ts
+++ packages/web/src/lib/api/server.ts
@@ -11,6 +11,7 @@ export const fetchServer = createFetchClient<paths>({
         cookie: (await cookies()).toString(),
       },
       cache: "no-store",
+      credentials: "include",
     });
   },
 });
