d78e93b41ad5aaca1ee605b919aef58dd9d814fa
microhum <Guntee12123@gmail.com>
2025-08-02 18:30:42 +0700

    chore: add file title showing

--- packages/web/src/app/(protected)/collection/[id]/docs/[docsId]/page.tsx
+++ packages/web/src/app/(protected)/collection/[id]/docs/[docsId]/page.tsx
@@ -86,7 +86,7 @@ function DocIdPage() {
                     />
                     Overview
                   </TabsTrigger>
-                  <TabsTrigger value="tab-2" className="group" disabled={true}>
+                  <TabsTrigger value="tab-2" className="group">
                     <PanelsTopLeftIcon
                       className="-ms-0.5 me-1.5 opacity-60"
                       size={16}
